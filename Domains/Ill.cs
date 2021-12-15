7d2f06d068ab49486548e1a58f28751a94200ebb
iluxa678 <ilyaabramov678@gmail.com>
2021-12-15 11:25:14 +0300

    new1

--- Domains/Ill.cs
+++ Domains/Ill.cs
@@ -5,12 +5,7 @@
         public int IllId { get; set; }
         public string IllName { get; set; }
         public string DescriptionIll { get; set; }
-<<<<<<< HEAD
         public Diag Diag { get; set; }      
         }  
 }
-=======
-        public Diag Diag { get; set; }
-    }
-}
->>>>>>> 9a2f91c7fd3f01cb8da6fa9539724087f1302b12
+
